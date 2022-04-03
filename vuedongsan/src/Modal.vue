82764a2cd2c482b8d0c58629d5fb337d217853de
choijaeho <dhqkgbs@naver.com>
2022-04-03 22:45:43 +0900

    vue input watch 사용법

--- vuedongsan/src/Modal.vue
+++ vuedongsan/src/Modal.vue
@@ -8,7 +8,7 @@
       <!-- <input @input="month = $event.target.value"> input 받는거 @input 할때마다 , @change 커서다른대 찍으면 -->
       <input v-model="month"> <!--여기에 입력한걸 밑에 저장-->
       <!--수량은 바로 저장 $event e랑 똑같은 파라미터 역할-->
-       <!--초기값이 중요, 문자 or 숫자-->
+      <!--초기값이 중요, 문자 or 숫자-->
       <p>  {{month}}개월 선택함 : 가격 : {{ onerooms[clickIndex].price * month}}원 </p>
       <button @click="close">닫기</button>
       <!-- v-model -->
@@ -16,6 +16,7 @@
   </div>
 </template>
 
+<!-- Watcher : data 감시하는 함수 /  input을 받는거라면 거의 필요함.-->
 <script>
 export default{
     name : "TheModal",
@@ -25,8 +26,30 @@ export default{
         //props는 수정이 불가능하다. const같음. read only데이터라고 보면됨.
         //부모의 데이터를 수정하고싶으면 custom event를 사용해야됨.
         return{
-            month : 1
+            month : 1,
         } 
+    },
+    //데이터를 감시하는 감시자
+    watch :{
+        //month에 숫자만 있는지 감시 
+        //month라는 이름으로 함수를 만든다. 
+        //month데이터가 변화할때마다, 이 함수가 쓱 쓰인다. 
+        month(a){ // a == 변경후 month임 , b == 변경전 데이터임
+            //사용자가 month를 글자로 입력하면, 경고문 띄움.
+            //사용자가 month에 입력한 데이터가 13보다 크면, 경고문 띄움
+            if(a > 12) {
+                alert("13개월이상 입력불가합니다.");
+                this.month = 1; 
+            }
+            //글자 check 글자가 아니면, month를 1로 변경해줌. 
+            if(isNaN(a) == true){
+                alert("문자열을 입력하면 안됩니다.");
+                this.month = 1; 
+            }
+
+            //체크할게 너무 많으면, Vue용 form validation라이브러리를 쓴다. 
+            
+        }
     },
     props :{
         onerooms :  Array ,//데이터 형식
